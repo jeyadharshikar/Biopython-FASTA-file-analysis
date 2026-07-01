f0c2dc7f8cc69b0f744d95bad98fb11a752a9161
jeyadharshikar <dhani.29.dharshi.14@gmail.com>
2026-07-01 08:34:18 +0530

    fasta transcribe.py

--- fasta transcribe.py
+++ fasta transcribe.py
@@ -3,4 +3,4 @@ from Bio import SeqIO
 for record in SeqIO.parse("homosapien.fasta", "fasta"):
 
     print("ID:", record.id)
-    print("Protein:", record.seq.transcribe())
+    print("RNA:", record.seq.transcribe())
